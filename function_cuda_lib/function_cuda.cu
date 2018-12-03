b7582abdd09dc485419cc4b018c7f88d493d440e
Kirill <pshcyrill@mail.ru>
2018-12-03 10:37:07 +0300

    small edit

--- function_cuda_lib/function_cuda.cu
+++ function_cuda_lib/function_cuda.cu
@@ -1,25 +1,106 @@
+/*
+ * Kirill Pshenichny aka tre3k (c) 11.2018
+ * This is part for calculationt the neutron scattering in the samples
+ * with RKKY interaction.
+ *
+ */
+
+
 #include <cuda.h>
 
 #include "function_cuda.h"
 
 /* *** GPU Functions *** */
 
-__device__ double func1(double *t_x,double *t_y,double *o){
-  double retval;
+__device__ double dE_i,dDeltaH,dA0,dk_s,dk_i;
 
+__device__ double *dMap;                      // variable for dispersion 
+
+
+struct sFunc{
+  double func1,func2;
+};
+
+__global__ void calcConst(){
+  dDeltaH = dDeltaH*P_g*P_uB;
+  dE_i = P_h*P_h*dk_i*dk_i/2/P_mn;
+  return;
+}
+
+__device__ struct sFunc func(double *lt_x,double *lt_y,double *lo){
+  struct sFunc retval;
+
+  double o = *lo;
+  double t_x = *lt_x;
+  double t_y = *lt_y;
+
+  double o2 = o*o;
+  double t_x2 = t_x*t_x;
+  double t_y2 = t_y*t_y;
+
+  double A = t_x2*dk_i*dk_i;
+  double B = t_y2*dk_i*dk_i;
+  double C = P_h*P_h*o2*dk_i*dk_i/4/dE_i/dE_i;
+  double D = dk_s*dk_s; 
+
+  retval.func1 = o - dA0*(A+B+C-D)*(A+B+C-D) - dDeltaH;
+  retval.func2 = o + dA0*(A+B+C-D)*(A+B+C-D) + dDeltaH;
   
   return retval;
+}
+
+__global__ void generateMap(double *from_var, double *to_var, int *N){
+  dMap = new double [*N];
+  double dvar = (*to_var - *from_var)/ (double)(*N);
+  for(int i=0;i<*N;i++) dMap[i] = *from_var + (double)i*dvar;
+}
+
+__global__ void deleteMap(){
+  delete [] dMap;
+}
+
+__global__ void dispersionAtTheta_x(double *out){
+  
+  return;
 }
 
 __global__ void func_test(double *in,double *out){
   int tid = blockIdx.x;
   out[tid] = in[tid]*in[tid];
+
   return;
 }
 
 
 
 /* *** CPU Functions *** */
+
+void FunctionCuda::memcpyConst(){
+
+  double *lA0,*lDeltaH,*lk_i,*lk_s;
+  lA0 = &dA0;
+  lDeltaH = &dDeltaH;
+  lk_i = &dk_i;
+  lk_s = &dk_s;
+
+  cudaMemcpy(lA0, &A, sizeof(double), cudaMemcpyHostToDevice);
+  cudaMemcpy(lDeltaH, &DeltaH, sizeof(double), cudaMemcpyHostToDevice);
+  cudaMemcpy(lk_i, &k_i, sizeof(double), cudaMemcpyHostToDevice);
+  cudaMemcpy(lk_s, &k_s, sizeof(double), cudaMemcpyHostToDevice);
+  
+  calcConst<<<1,1>>>();
+  
+  return;
+}
+
+void FunctionCuda::getDispersion(double *data1,double *data2,
+				 int flags, double from_var, double to_var, int N){
+
+  memcpyConst();
+
+  
+  return;
+}
 
 double *FunctionCuda::test(double *in,int N){
   
--- function_cuda_lib/function_cuda.h
+++ function_cuda_lib/function_cuda.h
@@ -1,17 +1,34 @@
 #ifndef FUNCTION_CUDA_H
 #define FUNCTION_CUDA_H
 
+/* flags */
+#define F_DISPERSION_AT_THETA_X  0
+#define F_DISPERSION_AT_THETA_Y  1
+#define F_DISPERSION_AT_OMEGA    2
+
+/* physical constant */
+#define P_h            1.054571800e-34     // Joul*s  plank const
+#define P_mn           1.674927471e-27     // kg  neutron mass
+#define P_uB           927.400968e-26      // Joul/T  Bor magneton
+#define P_g            -2.00231930436153   // g-factor
+#define P_e            1.60217662e-19      // e Kulon
+
 class FunctionCuda{
  public:
   double A;                 // Stiffness RKKY
   double DeltaH;            // (H-Hc2)
   double k_i;               // spiral wave vector
+  double k_s;
   
   double *test(double *in,int N);
   void getRoot1(double *t_x,double *t_y, double *omega);
   void getRoot2(double *t_x,double *t_y, double *omega);
+
+  void getDispersion(double *data1,double *data2,
+		     int flags, double from_var, double to_var, int N);
   
  private:
+  void memcpyConst(void);
   
 };
 
